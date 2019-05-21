3c259cdd04a872ea0ad81723c1f59d33265510ff
397383523@qq.com <8362781wj>
2019-05-21 16:05:39 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/MyApplication.java
+++ app/src/main/java/com/wj/wandroid/MyApplication.java
@@ -2,6 +2,8 @@ package com.wj.wandroid;
 
 import android.app.Application;
 
+import com.lzy.okgo.OkGo;
+
 /**
  * author wangjing
  * Date 2018/11/29
@@ -11,5 +13,7 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        OkGo.getInstance().init(this);
     }
 }
