76e2c70a2d4e593b2ffbfc08f708e14864aac475
397383523@qq.com <8362781wj>
2019-05-21 14:41:02 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/activity/MainActivity.java
+++ app/src/main/java/com/wj/wandroid/activity/MainActivity.java
@@ -1,15 +1,49 @@
 package com.wj.wandroid.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.wj.wandroid.R;
+import com.wj.wandroid.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends BaseActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+        button = findViewById(R.id.button);
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setEvent() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doSomeThing();
+            }
+        });
+    }
+
+    private void doSomeThing() {
+
     }
 }
