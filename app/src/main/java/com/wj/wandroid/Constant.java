daba2a5d1a0ca211e0d9ff192c4947a249991fa2
397383523@qq.com <8362781wj>
2019-05-21 15:49:55 +0800

    1111111111

---
+++ app/src/main/java/com/wj/wandroid/Constant.java
@@ -0,0 +1,10 @@
+package com.wj.wandroid;
+
+/**
+ * author wangjing
+ * Date 2019/5/21
+ * Description
+ */
+public class Constant {
+    public static final String HOST_URL = "https://www.wanandroid.com";
+}
