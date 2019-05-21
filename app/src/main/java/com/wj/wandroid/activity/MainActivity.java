ab37acca244e7df46b56c6282957207de3a565a3
397383523@qq.com <8362781wj>
2019-05-21 17:01:19 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/activity/MainActivity.java
+++ app/src/main/java/com/wj/wandroid/activity/MainActivity.java
@@ -1,6 +1,5 @@
 package com.wj.wandroid.activity;
 
-import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.view.ViewPager;
@@ -9,7 +8,6 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
 
 import com.scwang.smartrefresh.layout.SmartRefreshLayout;
 import com.wj.wandroid.R;
