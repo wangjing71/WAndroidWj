6613a1121788e874adc32b2fd4c7dcdb2303957c
397383523@qq.com <8362781wj>
2019-05-21 15:58:21 +0800

    1111111111

---
+++ app/src/main/java/com/wj/wandroid/util/HttpRequestUtils.java
@@ -0,0 +1,52 @@
+package com.wj.wandroid.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.cache.CacheMode;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
+import com.wj.wandroid.Constant;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * author wangjing
+ * Date 2018/11/20
+ * Description
+ */
+public class HttpRequestUtils {
+
+    public interface StringCallBack {
+        void onSuccess(String result);
+        void onFail();
+    }
+
+    public static void get(Context context, String path, final StringCallBack callback) {
+        OkGo.<String>get(Constant.HOST_URL+path)                            // 请求方式和请求url
+                .cacheMode(CacheMode.DEFAULT)    // 缓存模式，详细请看缓存介绍
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
+                        Log.i("====",response.body());
+                        callback.onSuccess(response.body());
+                    }
+
+                    @Override
+                    public void onError(Response<String> response) {
+                        super.onError(response);
+                        callback.onFail();
+                    }
+                });
+    }
+}
