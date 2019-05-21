27a4d290a9547392b16856fa1b1d2fbabfb276d8
397383523@qq.com <8362781wj>
2019-05-21 15:15:10 +0800

    1111111111

---
+++ app/src/main/java/com/wj/wandroid/activity/ImageAdActivity.java
@@ -0,0 +1,31 @@
+package com.wj.wandroid.activity;
+
+import com.wj.wandroid.R;
+import com.wj.wandroid.base.BaseActivity;
+
+/**
+ * author wangjing
+ * Date 2019/5/21
+ * Description
+ */
+public class ImageAdActivity extends BaseActivity {
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_image_ad;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setEvent() {
+
+    }
+}
