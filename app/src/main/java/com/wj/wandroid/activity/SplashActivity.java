d0c3afa094e569dee4044a81e19dd37ea7112b6a
397383523@qq.com <8362781wj>
2019-05-21 17:11:29 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/activity/SplashActivity.java
+++ app/src/main/java/com/wj/wandroid/activity/SplashActivity.java
@@ -1,11 +1,17 @@
 package com.wj.wandroid.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.RequiresApi;
 
+import com.tbruyelle.rxpermissions2.RxPermissions;
 import com.wj.wandroid.R;
 import com.wj.wandroid.base.BaseActivity;
+
+import java.util.function.Consumer;
 
 /**
  * author wangjing
@@ -32,9 +38,17 @@ public class SplashActivity extends BaseActivity {
         handler.sendEmptyMessageDelayed(0,2000);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void initData() {
-
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(Manifest.permission.READ_PHONE_STATE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
+                .subscribe(new Consumer<Boolean>() {
+                    @Override
+                    public void accept(Boolean aBoolean) {
+                    }
+                });
     }
 
     @Override
