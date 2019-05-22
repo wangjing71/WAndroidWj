bc0d6f47e658f9ec0055f493f5194b65b1a27137
397383523@qq.com <8362781wj>
2019-05-22 09:23:05 +0800

    1111111111

---
+++ app/src/main/java/com/wj/wandroid/activity/WebActivity.java
@@ -0,0 +1,31 @@
+package com.wj.wandroid.activity;
+
+import com.wj.wandroid.R;
+import com.wj.wandroid.base.BaseActivity;
+
+/**
+ * author wangjing
+ * Date 2019/5/22
+ * Description
+ */
+public class WebActivity extends BaseActivity {
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_web;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setEvent() {
+
+    }
+}
