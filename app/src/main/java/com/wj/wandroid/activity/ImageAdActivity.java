3a817bb8df46c139e94bb95212ab7d0e6aab837b
397383523@qq.com <8362781wj>
2019-05-23 15:12:36 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/activity/ImageAdActivity.java
+++ app/src/main/java/com/wj/wandroid/activity/ImageAdActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.wj.wandroid.Constant;
 import com.wj.wandroid.R;
 import com.wj.wandroid.base.BaseActivity;
+import com.wj.wandroid.bean.ImageBean;
 import com.wj.wandroid.util.HttpRequestUtils;
+
+import java.util.Random;
 
 /**
  * author wangjing
@@ -53,7 +58,12 @@ public class ImageAdActivity extends BaseActivity {
         HttpRequestUtils.get(Constant.SPLASH_IMAGE, new HttpRequestUtils.StringCallBack() {
             @Override
             public void onSuccess(String result) {
-                Log.i("====",result);
+                ImageBean imageBean = gson.fromJson(result,ImageBean.class);
+                int count = imageBean.getData().size();
+                Random r=new Random();
+                int i1=r.nextInt(count-1);    //生成[0,10]区间的整数
+                String url = imageBean.getData().get(i1).getImage_url();
+                Glide.with(ImageAdActivity.this).load(url).into(adImg);
             }
 
             @Override
