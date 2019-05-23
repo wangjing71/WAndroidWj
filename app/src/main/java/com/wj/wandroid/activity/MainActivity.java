a1fda94678eecc9abda526003cd9e1a61ee7918e
397383523@qq.com <8362781wj>
2019-05-23 16:48:30 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/activity/MainActivity.java
+++ app/src/main/java/com/wj/wandroid/activity/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImmersionBar.with(this).barColor(R.color.red_primary).init();
         initView();
         initData();
         setEvent();
