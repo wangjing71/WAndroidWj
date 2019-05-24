ee04796ece7d7d467183e19efab054f619462f15
397383523@qq.com <8362781wj>
2019-05-24 14:42:20 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/fragment/BaseFragment.java
+++ app/src/main/java/com/wj/wandroid/fragment/BaseFragment.java
@@ -2,6 +2,7 @@ package com.wj.wandroid.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,89 +15,40 @@ import android.view.ViewGroup;
  * Description
  */
 public abstract class BaseFragment extends Fragment {
-    protected View mRootView;
-    protected Context mContext;
-    protected boolean isVisible;
     private boolean isPrepared;
-    private boolean isFirst = true;
+    private boolean isLazyLoaded;
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         isPrepared = true;
-        initPrepare();
+        lazyLoad();
     }
 
     @Override
     public void setUserVisibleHint(boolean isVisibleToUser) {
         super.setUserVisibleHint(isVisibleToUser);
-        if(getUserVisibleHint()){
-            isVisible = true;
-            lazyLoad();
-        }else{
-            isVisible = false;
-            onInvisible();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if(getUserVisibleHint()){
-            setUserVisibleHint(true);
-        }
-    }
-
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mContext = getActivity();
+        lazyLoad();
     }
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        if(mRootView == null){
-            mRootView = initView(inflater,container,savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        isPrepared = true;
+        View view = inflater.inflate(setContentViewId(), null, false);
+        return view;
+    }
+    public abstract int setContentViewId();
+
+    private void lazyLoad() {
+        if (getUserVisibleHint() && isPrepared && !isLazyLoaded) {
+            onLazyLoad();
+            isLazyLoaded = true;
         }
-        return mRootView;
     }
 
-    //--------------------------------method---------------------------//
+    private void onLazyLoad() {
 
-    /**
-     * 懒加载
-     */
-    protected void lazyLoad(){
-        if(!isPrepared || !isVisible || !isFirst){
-            return;
-        }
-        initData();
-        isFirst = false;
+
     }
-
-    //--------------------------abstract method------------------------//
-
-    /**
-     * 在onActivityCreated中调用的方法，可以用来进行初始化操作。
-     */
-    protected abstract void initPrepare();
-
-    /**
-     * fragment被设置为不可见时调用
-     */
-    protected abstract void onInvisible();
-
-    /**
-     * 这里获取数据，刷新界面
-     */
-    protected abstract void initData();
-
-    /**
-     * 初始化布局，请不要把耗时操作放在这个方法里，这个方法用来提供一个
-     * 基本的布局而非一个完整的布局，以免ViewPager预加载消耗大量的资源。
-     */
-    protected abstract View initView(LayoutInflater inflater,
-                                     @Nullable ViewGroup container,
-                                     @Nullable Bundle savedInstanceState);
 }
