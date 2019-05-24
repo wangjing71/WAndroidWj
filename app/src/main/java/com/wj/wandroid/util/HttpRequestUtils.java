3dfbdc0f22508963315a0f9936f715e55ccf96d7
397383523@qq.com <8362781wj>
2019-05-24 09:37:28 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/util/HttpRequestUtils.java
+++ app/src/main/java/com/wj/wandroid/util/HttpRequestUtils.java
@@ -29,6 +29,7 @@ public class HttpRequestUtils {
                 .execute(new StringCallback() {
                     @Override
                     public void onSuccess(Response<String> response) {
+                        Log.i("====出参",response.body());
                         callback.onSuccess(response.body());
                     }
 
