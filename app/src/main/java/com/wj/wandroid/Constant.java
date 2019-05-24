c97bb890a39ecc01031c38064a951c35dfc1674e
397383523@qq.com <8362781wj>
2019-05-24 16:16:06 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/Constant.java
+++ app/src/main/java/com/wj/wandroid/Constant.java
@@ -6,5 +6,5 @@ package com.wj.wandroid;
  * Description
  */
 public class Constant {
-    public static final String SPLASH_IMAGE = "http://image.baidu.com/channel/listjson?pn=%number&rn=30&tag1=美女&tag2=%type&ftags=%type&ie=utf8";
+    public static final String SPLASH_IMAGE = "http://image.baidu.com/channel/listjson?pn=%number&rn=30&tag1=美女&tag2=%type&ie=utf8";
 }
