ec5c4c0c2ca7d42cafc3f320a1a0c223bdece3fe
397383523@qq.com <8362781wj>
2019-06-03 14:14:28 +0800

    1111111111

---
+++ app/src/main/java/com/wj/wandroid/activity/ImageDetailActivity.java
@@ -0,0 +1,31 @@
+package com.wj.wandroid.activity;
+
+import com.wj.wandroid.R;
+import com.wj.wandroid.base.BaseActivity;
+
+/**
+ * author wangjing
+ * Date 2019/6/3
+ * Description
+ */
+public class ImageDetailActivity extends BaseActivity {
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_image_detail;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void setEvent() {
+
+    }
+}
