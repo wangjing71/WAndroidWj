733c7b828e9c80d331186054590ef32299d079b5
397383523@qq.com <8362781wj>
2019-06-24 10:31:34 +0800

    1111111111

---
+++ app/src/main/java/com/wj/wandroid/base/BaseLazyFragment.java
@@ -0,0 +1,128 @@
+package com.wj.wandroid.base;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class BaseLazyFragment extends Fragment {
+    public Activity activity;
+    private boolean isFragmentVisible;
+    private boolean isPrepared;
+    private boolean isFirstLoad = true;
+    private boolean forceLoad = false;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        if (bundle != null && bundle.size() > 0) {
+            initVariables(bundle);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        isFirstLoad = true;
+        View view = View.inflate(activity, setContentViewId(), null);
+        isPrepared = true;
+        initView(view);
+        lazyLoad();
+        return view;
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        if (getUserVisibleHint()) {
+            onVisible();
+        } else {
+            onInvisible();
+        }
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (!hidden) {
+            onVisible();
+        } else {
+            onInvisible();
+        }
+    }
+
+    protected void onVisible() {
+        isFragmentVisible = true;
+        lazyLoad();
+    }
+
+    protected void onInvisible() {
+        isFragmentVisible = false;
+    }
+
+    protected void lazyLoad() {
+        if (isPrepared() && isFragmentVisible()) {
+            if (forceLoad || isFirstLoad()) {
+                forceLoad = false;
+                isFirstLoad = false;
+                initData();
+                setEvent();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        isPrepared = false;
+    }
+
+    public void initVariables(Bundle bundle) { }
+
+    public void setEvent() { }
+
+    public abstract int setContentViewId();
+
+    public abstract void initView(View root);
+
+    protected abstract void initData();
+
+    public boolean isPrepared() {
+        return isPrepared;
+    }
+
+    public boolean isFirstLoad() {
+        return isFirstLoad;
+    }
+
+    public boolean isFragmentVisible() {
+        return isFragmentVisible;
+    }
+
+    @TargetApi(23)
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        onAttachToContext(context);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            onAttachToContext(activity);
+        }
+    }
+
+    protected void onAttachToContext(Context context) {
+        activity = getActivity();
+    }
+
+}
