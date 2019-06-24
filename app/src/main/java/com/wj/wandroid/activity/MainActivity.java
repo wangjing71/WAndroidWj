d5f96fb8542ce42286fed06bd9ec955744b903ad
397383523@qq.com <8362781wj>
2019-06-24 10:32:09 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/activity/MainActivity.java
+++ app/src/main/java/com/wj/wandroid/activity/MainActivity.java
@@ -1,15 +1,20 @@
 package com.wj.wandroid.activity;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 
 import com.wj.wandroid.R;
 import com.wj.wandroid.adapter.HomeViewPagerAdapter;
 import com.wj.wandroid.base.BaseActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends BaseActivity {
 
     private ViewPager mViewPager;
     private HomeViewPagerAdapter homeViewPagerAdapter;
+    private List<Fragment> fragmentList = new ArrayList<>();
 
     @Override
     protected int setLayoutId() {
@@ -23,7 +28,7 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-        homeViewPagerAdapter = new HomeViewPagerAdapter(getSupportFragmentManager(),,);
+        homeViewPagerAdapter = new HomeViewPagerAdapter(getSupportFragmentManager(), , );
     }
 
     @Override
