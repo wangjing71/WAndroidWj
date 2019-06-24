480c4b1a235962867b1774a1539c28c87dc7737c
397383523@qq.com <8362781wj>
2019-06-24 10:14:37 +0800

    1111111111

---
+++ app/src/main/java/com/wj/wandroid/base/BaseFragment.java
@@ -0,0 +1,11 @@
+package com.wj.wandroid.base;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * author wangjing
+ * Date 2019/6/24
+ * Description
+ */
+public class BaseFragment extends Fragment {
+}
