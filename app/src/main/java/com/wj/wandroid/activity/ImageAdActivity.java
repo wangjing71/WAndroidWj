6f32f99dbc5a5b8f5d296258b62c612336dcf011
397383523@qq.com <8362781wj>
2019-06-25 14:41:52 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/activity/ImageAdActivity.java
+++ app/src/main/java/com/wj/wandroid/activity/ImageAdActivity.java
@@ -1,24 +1,36 @@
 package com.wj.wandroid.activity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.Nullable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.gyf.immersionbar.ImmersionBar;
 import com.wj.wandroid.R;
-import com.wj.wandroid.base.BaseActivity;
 
 /**
  * author wangjing
  * Date 2019/5/21
  * Description
  */
-public class ImageAdActivity extends BaseActivity {
+public class ImageAdActivity extends Activity {
     private ImageView adImg;
     private TextView skip;
     private int time = 3;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(setLayoutId());
+        ImmersionBar.with(this).init();
+        initView();
+        initData();
+        setEvent();
+    }
 
     private Handler handler = new Handler() {
         @Override
@@ -32,12 +44,10 @@ public class ImageAdActivity extends BaseActivity {
         }
     };
 
-    @Override
     protected int setLayoutId() {
         return R.layout.activity_image_ad;
     }
 
-    @Override
     protected void initView() {
         adImg = findViewById(R.id.ad_img);
         skip = findViewById(R.id.skip);
@@ -45,11 +55,9 @@ public class ImageAdActivity extends BaseActivity {
         handler.sendEmptyMessageDelayed(0, 1000);
     }
 
-    @Override
     protected void initData() {
     }
 
-    @Override
     protected void setEvent() {
 
     }
