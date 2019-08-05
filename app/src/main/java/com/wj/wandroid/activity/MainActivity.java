ce00e263fb85b8202ca4301742e8ad975d334774
397383523@qq.com <8362781wj>
2019-08-05 14:32:08 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/activity/MainActivity.java
+++ app/src/main/java/com/wj/wandroid/activity/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.bumptech.glide.Glide;
 import com.wj.wandroid.R;
 import com.wj.wandroid.adapter.HomeViewPagerAdapter;
 import com.wj.wandroid.base.BaseActivity;
 import com.wj.wandroid.fragment.HomePageFragment;
+import com.wj.wandroid.util.GlideUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,9 +24,9 @@ public class MainActivity extends BaseActivity {
     private List<Fragment> fragmentList = new ArrayList<>();
     private LinearLayout tabMenu;
     private int[] defaultIconList =
-            new int[]{R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
+            new int[]{R.mipmap.home_def, R.mipmap.activity_def, R.mipmap.message_def, R.mipmap.mine_def};
     private int[] focusIconList =
-            new int[]{R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher};
+            new int[]{R.mipmap.home_foc, R.mipmap.activity_foc, R.mipmap.message_foc, R.mipmap.mine_foc};
     @Override
     protected int setLayoutId() {
         return R.layout.activity_main;
@@ -48,9 +51,13 @@ public class MainActivity extends BaseActivity {
     }
 
     private void initTabMenu() {
-        View items = LayoutInflater.from(this).inflate(R.layout.tab_menu_item, null, false);
         for (int i = 0; i < 4; i++) {
-
+            View items = LayoutInflater.from(this).inflate(R.layout.tab_menu_item, null, false);
+            ImageView focused_icon = items.findViewById(R.id.focused_icon);
+            ImageView default_icon = items.findViewById(R.id.default_icon);
+            Glide.with(this).load(defaultIconList[i]).into(focused_icon);
+            Glide.with(this).load(focusIconList[i]).into(focused_icon);
+            tabMenu.addView(items);
         }
     }
 
