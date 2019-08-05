1d6908d9d772dae2f4aa4c14488a6735d32e82b1
397383523@qq.com <8362781wj>
2019-08-05 16:47:35 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/fragment/HomePageFragment.java
+++ app/src/main/java/com/wj/wandroid/fragment/HomePageFragment.java
@@ -4,6 +4,7 @@ import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -43,7 +44,8 @@ public class HomePageFragment extends BaseLazyFragment {
     @Override
     protected void initData() {
         for (int i = 0; i < 4; i++) {
-
+            View page1 = LayoutInflater.from(getContext()).inflate(R.layout.home_banner_item,null,false);
+            viewList.add(page1);
         }
     }
 
