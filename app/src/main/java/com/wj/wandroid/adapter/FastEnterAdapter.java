aa45ef72afb1c198813383a0890dd72961d5a84d
397383523@qq.com <8362781wj>
2019-08-05 17:26:22 +0800

    1111111111

--- app/src/main/java/com/wj/wandroid/adapter/FastEnterAdapter.java
+++ app/src/main/java/com/wj/wandroid/adapter/FastEnterAdapter.java
@@ -58,15 +58,4 @@ public class FastEnterAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         void itemClick(int position);
     }
 
-    public SpannableString matcherSearchText(int color, String text, String keyword) {
-        SpannableString ss = new SpannableString(text);
-        Pattern pattern = Pattern.compile(keyword);
-        Matcher matcher = pattern.matcher(ss);
-        while (matcher.find()) {
-            int start = matcher.start();
-            int end = matcher.end();
-            ss.setSpan(new ForegroundColorSpan(color), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-        }
-        return ss;
-    }
 }
